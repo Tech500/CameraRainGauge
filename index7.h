8a010520ef6e88a37c29b352438b346d30c1ae5e
William Lucid <11734452+Tech500@users.noreply.github.com>
2023-07-30 17:49:54 -0400

    Update index7.h

--- index7.h
+++ index7.h
@@ -1,59 +1,73 @@
-//index6.h
+//index7.h
 const char HTML7[] PROGMEM = R"====(
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+  html, body {
+  margin: 0; /* Set margin to 0 to remove any unwanted spacing */
+  padding: 0;
+  height: 100%;
+}
 
-  <meta charset="utf-8">  
-  <title>Unreal Media Server WebRTC player</title>
-  
-  <script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=5_kmXRo0UtYPkMmvi2pueMJ6TwZtFe75E64hAzwNX3T7zLrRHvxN1HY6Y5-TWOn1wOXmCU82BwjNFhbXOfXXoZzIOwXMUFzny9SKZ2ckHOwGLIDBjuLnuE1JrU5lRPY9" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="http://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cDovL3d3dy51bWVkaWFzZXJ2ZXIubmV0L3VtZWRpYXNlcnZlci9kZW1vaHRtbDVXZWJSVENwbGF5ZXIuaHRtbA"/><script src="http://www.umediaserver.net/umediaserver/webrtcadapter.js"></script>
-  <script src="http://www.umediaserver.net/umediaserver/unrealwebrtcplayer.js"></script>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh; /* Use viewport height for container */
+}
 
-    <script type="text/javascript">
+  @media (max-width: 768px) {
+  .container {
+    flex: 1 1 auto;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+}
+    
+    header {
+      margin: 5vh auto 0;
+      width: 100%;
+      text-align: center;
+    }
+    
+    main {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    
+    iframe {
+      aspect-ratio: 16 / 9;
+      width: 1300px; /* Set the width to 100% to occupy the entire container */
+      flex: 1; /* Use flex: 1 to make the iframe expand and fill the available space */
+    }
 
-      var webrtcPlayer = null;
-
-    </script>
-
+    footer {
+      width: 100%;
+      text-align: center;
+      padding: 10px; /* Add some padding to the footer for spacing */
+    }
+</style>
 </head>
-
 <body>
-
-  <div id="container" align="center">
-  
-    <br><br><br>
-    Unreal Media Server WebRTC player<br><br>This player plays live near real time audio/video on any OS and mobile device, in all major browsers. This is original H264 video encoded by IP camera; server doesn't do any transcoding.
-    <br><br>Wyse Cam 3; providing RTSP video feed.
-     <!-- <A style="COLOR: darkgreen;" href="demohtml5WebRTCplayerSecure.html">Same page with signaling over secure WebSocket</A> -->
-    <br><br><br><br><br><br><br><br>
-        
-    <video style="background-color:black" id="remoteVideo" width="800" height="450" autoplay playsinline controls muted></video>
-
-    <script type="text/javascript">
-
-        //Parameters:
-
-        //1. ID of video element.
-        //2. Alias of the live broadcast; video encoding supported: H264(AVC1), VP8, VP9; audio encoding supported: opus, G.711(PCMA/PCMU).
-        //3. Secure token for session based authentication, leave empty if none. Should not be used unless Media Server and Web Server run on different domains.
-        //4. IP address of Unreal Media Server. If you specify domain name (domain name must be used for signaling over secure websocket) AND your server is behind NAT, then you need to make a change in unrealwebrtcplayer.js, described in http://www.umediaserver.net/phpBB3/viewtopic.php?f=29&t=3563
-        //5. Main port of Unreal Media Server for signaling over websocket (by default, 5119 for unsecure webscokets / 443 for secure websockets). Make sure these ports are open in the firewall.
-        //6. Use secure websocket for signaling; if true, make sure to set the port to secure port (443 by default) and configure SSL certificate in Unreal Media Server. If this parameter is false and this webpage is loaded under https:// then browsers will display "mixed content" warning; you will need to allow mixed content in your browser.
-        //7. Use single central port for actual WebRTC ICE connection. If true, the web browser will receive streaming media from predefined single port in Unreal Media Server, dedicated to WebRTC connections (by default 5135). If true, you only need to open port 5135 in firewall/NAT router for Unreal Media Server (together with signaling ports, 5119 or 443). If false, the WebRTC connection will be made over a random port; you will need to open all ports in the firewall, or add Unreal Media Server as an allowed app in the firewall.
-        //8. WebRTC transport protocol. Set to "udp" or "tcp". The central port can be used for both udp and tcp.
-
-        webrtcPlayer = new UnrealWebRTCPlayer("remoteVideo", "replace_alias", "", "replace_ipAddress of server", "5119", false, true, "tcp");
-
-        //Comment out next line not to start playing when webpage loads. Then user will need to click on Play button to play; you may want to use a video element with overlayed Play button - check out our SDK for sample webpages.
-        webrtcPlayer.Play();    //Start playing automatically when webpage loads. Notice that video element has a "muted" attribute; this is video-only stream anyway. A muted attribute helps to overcome Chrome's autoplay policy, and is not always needed, as described in http://www.umediaserver.net/phpBB3/viewtopic.php?f=29&t=3578
-
-    </script>
-
-    <br><br><br><a href=http://%LINK%/Weather >Main Menu</a></h2>   
-
+  <div class="container">
+    <header>
+      <br><br>This is original H264 video encoded by IP camera; server doesn't do any transcoding.
+      <br>Wyse Cam v3; providing webRTC video feed.  Camera maybe offline; depending on battery discharge state.
+      <br><br>
+    </header>
+    <main>
+      <iframe class="iframe" width="1300" height="731"src="http://68.45.250.167:8889/backyard" frameborder="0"></iframe> 
+    </main>
+    <footer>
+      <h2><a href="http://%LINK%/Weather" >ESP32 Server</a></h2>  
+    </footer>
   </div>
-
 </body>
 </html>
 )====";
